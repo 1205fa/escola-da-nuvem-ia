9f045a5b35b98bb0d8506babe6435d149c2ab985
Seu Nome <fcalberico77@gmail.com141343060+1205fa@users.noreply.github.com>
2024-11-20 19:41:48 -0300

    feat: Criando operadores Aritméticos

---
+++ anotacoes/aula01/exemplos/aula02.py
@@ -0,0 +1,10 @@
+x = 10
+y = 3.14
+z = "número"
+z = 15
+z  = 4.15
+
+print(type(x))
+print(type(y))
+print(type(z))
+
---
+++ anotacoes/aula01/exemplos/operadores_aritmeticos.py
@@ -0,0 +1,8 @@
+a, b = 10, 3
+
+print(f"Adição: {a + b} ")
+print(f"Subtração: {a - b} ")
+print(f"Multiplicação: {a * b} ")
+print(f"Divisão: {a / b:.3f} ")
+print(f"Divisão inteira: {a // b} ")
+print(f"Modulo: {a % b} ")
