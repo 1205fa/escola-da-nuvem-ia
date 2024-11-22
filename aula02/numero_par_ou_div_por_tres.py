e186adb096c53f4379004a38c224c617cfcb1984
Seu Nome <fcalberico77@gmail.com141343060+1205fa@users.noreply.github.com>
2024-11-22 10:48:53 -0300

    feat: Criando calculo simples e salario com bonus

---
+++ aula02/numero_par_ou_div_por_tres.py
@@ -0,0 +1,6 @@
+#enquanto o número(index) estiver entre(1 e 100)
+#Se o número for divisivel por dois com resto zero ou divisivel por três com resto 
+
+for i in range(1,100):
+    if i % 10 == 0:
+        print(f"O número{i} é divisivel por 10")
